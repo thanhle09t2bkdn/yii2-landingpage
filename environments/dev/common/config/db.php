d4fcd889fae3b9495597e2b45aa035b4485a685e
thanhle <thanhle09t2bkdn@gmail.com>
2015-11-28 11:28:52 +0700

    fix email

--- environments/dev/common/config/db.php
+++ environments/dev/common/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=interview-app',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-landingpage',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
--- environments/dev/common/config/mail.php
+++ environments/dev/common/config/mail.php
@@ -2,14 +2,9 @@
 
 return [
     'class' => 'yii\swiftmailer\Mailer',
-    'viewPath' => '@common/mail',            
-    'transport' => [
-        'class' => 'Swift_SmtpTransport',
-        'host' => 'dev.co-mit.com',
-        'username' => 'dev01',
-        'password' => '8m#*4%#=rhKSN8qA',
-        'port' => '587',
-        'encryption' => 'tls'
-    ],
+    // send all mails to a file by default. You have to set
+    // 'useFileTransport' to false and configure a transport
+    // for the mailer to send real emails.
+    'useFileTransport' => false,
     //'useFileTransport'=>true,
 ];
